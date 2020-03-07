d52b0290a7b76b77075aea7cf7fca65b59946e6a
lies046 <skytree.skysky@gmail.com>
2020-03-07 20:11:08 +0900

    initial commit

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,32 @@
+class MembersController < ApplicationController
+
+  def index
+    @members = Member.order("number") end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
+  end
+
+  def create
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
+    @members = Member.search(params[:q])
+      render "index"
+  end
+  
+end
