4a5f680e24c73084efff84a1eff94766b4220df7
lies046 <skytree.skysky@gmail.com>
2020-03-09 11:38:08 +0900

    Add new and edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,16 @@ class ArticlesController < ApplicationController
     @articles = Article.order(released_at: :desc)
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 end
