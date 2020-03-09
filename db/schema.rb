2a15374fcc6ac0c30c3ca7ce21cc7abd71da4862
lies046 <skytree.skysky@gmail.com>
2020-03-09 10:46:28 +0900

    Add article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_015829) do
+ActiveRecord::Schema.define(version: 2020_03_08_080046) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "released_at", null: false
+    t.boolean "member_only", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "expired_at"
+  end
 
   create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "number", null: false
